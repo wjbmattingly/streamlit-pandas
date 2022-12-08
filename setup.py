fa4cb0cba6828beafe22ba2dcc832798e4ebb332
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2022-12-08 09:54:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as f:
     LONG_DESCRIPTION = f.read()
 
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 DESCRIPTION = 'Create a Streamlit Pandas App'
 
 setup(
